4e158ab80eba29c99e4e40caa0eeedc0071ea439
Kyle <kiloloco@me.com>
2017-07-23 22:07:43 -0700

    Finished tutorial

---
+++ Pods/Target Support Files/Pods-Storage On Firebase/Pods-Storage On Firebase-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Storage_On_Firebase : NSObject
+@end
+@implementation PodsDummy_Pods_Storage_On_Firebase
+@end
--- Storage On Firebase/ViewController.swift
+++ Storage On Firebase/ViewController.swift
@@ -7,29 +7,55 @@
 //
 
 import UIKit
+import FirebaseStorage
 
 class ViewController: UIViewController {
     
     @IBOutlet weak var uploadImage: UIImageView!
     @IBOutlet weak var downloadImage: UIImageView!
     
+    let filename = "earth.jpg"
     
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+    var imageReference: StorageReference {
+        return Storage.storage().reference().child("images")
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
     
     @IBAction func onUploadTapped() {
+        guard let image = uploadImage.image else { return }
+        guard let imageData = UIImageJPEGRepresentation(image, 1) else { return }
+        
+        let uploadImageRef = imageReference.child(filename)
+        
+        let uploadTask = uploadImageRef.putData(imageData, metadata: nil) { (metadata, error) in
+            print("UPLOAD TASK FINISHED")
+            print(metadata ?? "NO METADATA")
+            print(error ?? "NO ERROR")
+        }
+        
+        uploadTask.observe(.progress) { (snapshot) in
+            print(snapshot.progress ?? "NO MORE PROGRESS")
+        }
+        
+        uploadTask.resume()
         
     }
     
     @IBAction func onDownloadTapped() {
+        let downloadImageRef = imageReference.child(filename)
+        
+        let downloadtask = downloadImageRef.getData(maxSize: 1024 * 1024 * 12) { (data, error) in
+            if let data = data {
+                let image = UIImage(data: data)
+                self.downloadImage.image = image
+            }
+            print(error ?? "NO ERROR")
+        }
+        
+        downloadtask.observe(.progress) { (snapshot) in
+            print(snapshot.progress ?? "NO MORE PROGRESS")
+        }
+        
+        downloadtask.resume()
         
     }
 }
