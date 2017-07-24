cdc5dd5edd032e317f350d409d85c42c5f921268
Kyle <kiloloco@me.com>
2017-07-23 17:24:39 -0700

    Starter Project

---
+++ Storage On Firebase/ViewController.swift
@@ -0,0 +1,36 @@
+//
+//  ViewController.swift
+//  Storage On Firebase
+//
+//  Created by Kyle Lee on 7/23/17.
+//  Copyright © 2017 Kyle Lee. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet weak var uploadImage: UIImageView!
+    @IBOutlet weak var downloadImage: UIImageView!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    
+    @IBAction func onUploadTapped() {
+        
+    }
+    
+    @IBAction func onDownloadTapped() {
+        
+    }
+}
+
